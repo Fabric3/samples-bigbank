f025535818073e5c6a7285138727ed272b73a674
jmarino <jim.marino@gmail.com>
2016-10-07 09:09:12 +0200

    upgrade to 3.0.0

--- payments/src/main/java/f3/CompositeProvider.java
+++ payments/src/main/java/f3/CompositeProvider.java
@@ -5,11 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.fabric3.api.annotation.model.Provides;
-import org.fabric3.api.binding.zeromq.builder.ZeroMQBindingDefinitionBuilder;
-import org.fabric3.api.binding.zeromq.model.ZeroMQBindingDefinition;
-import org.fabric3.api.model.type.builder.ChannelDefinitionBuilder;
+import org.fabric3.api.binding.zeromq.builder.ZeroMQBindingBuilder;
+import org.fabric3.api.binding.zeromq.model.ZeroMQBinding;
+import org.fabric3.api.model.type.builder.ChannelBuilder;
 import org.fabric3.api.model.type.builder.CompositeBuilder;
-import org.fabric3.api.model.type.component.ChannelDefinition;
+import org.fabric3.api.model.type.component.Channel;
 import org.fabric3.api.model.type.component.Composite;
 
 /**
@@ -22,8 +22,8 @@ public class CompositeProvider {
     public static Composite createComposite() {
         // Creates a channel bound to a ZeroMQ socket. Publishers and subscribers to the channel will communicate via a ZeroMQ PUB/SUB socket.
         List<String> addresses = Collections.singletonList("localhost:8383");
-        ZeroMQBindingDefinition binding = ZeroMQBindingDefinitionBuilder.newBuilder().address(addresses).build();
-        ChannelDefinition channel = ChannelDefinitionBuilder.newBuilder("FraudChannel").binding(binding).build();
+        ZeroMQBinding binding = ZeroMQBindingBuilder.newBuilder().address(addresses).build();
+        Channel channel = ChannelBuilder.newBuilder("FraudChannel").binding(binding).build();
         return CompositeBuilder.newBuilder(COMPOSITE).channel(channel).deployable().build();
     }
 }
